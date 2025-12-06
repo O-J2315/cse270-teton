2b5ac1db51d1004768f0be9eb171e82c72b50736
o-j2315 <jhernandezap23@gmail.com>
2025-12-05 23:17:19 -0700

    Refactor smoketest to improve assertions and wait conditions

--- tests/test_smoketest.py
+++ tests/test_smoketest.py
@@ -12,21 +12,22 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 
 
-def setup_method(self, method):
-  options = Options()
-  options.add_argument("--headless=new")
-  self.driver = webdriver.Chrome(options=options)
-  self.vars = {}
+class TestSmoketest():
+  def setup_method(self, method):
+    options = Options()
+    options.add_argument("--headless=new")
+    self.driver = webdriver.Chrome(options=options)
+    self.vars = {}
   
   def teardown_method(self, method):
     self.driver.quit()
   
   def test_1(self):
-    assert self.driver.find_element(By.CSS_SELECTOR, ".header-title > h1").text == "Teton Idaho"
     self.driver.get("http://127.0.0.1:5500/teton/1.6/index.html")
     self.driver.set_window_size(1140, 796)
     elements = self.driver.find_elements(By.CSS_SELECTOR, ".header-logo img")
     assert len(elements) > 0
+    assert self.driver.find_element(By.CSS_SELECTOR, ".header-title > h1").text == "Teton Idaho"
     assert self.driver.find_element(By.CSS_SELECTOR, ".header-title > h2").text == "Chamber of Commerce"
     assert self.driver.title == "Teton Idaho CoC"
   
@@ -68,8 +69,7 @@ def setup_method(self, method):
     self.driver.find_element(By.NAME, "biztitle").click()
     self.driver.find_element(By.NAME, "biztitle").send_keys("owner")
     self.driver.find_element(By.NAME, "submit").click()
-    elements = self.driver.find_elements(By.NAME, "email")
-    assert len(elements) > 0
+    WebDriverWait(self.driver, 30).until(expected_conditions.presence_of_element_located((By.NAME, "email")))
   
   def test_5(self):
     self.driver.get("http://127.0.0.1:5500/teton/1.6/index.html")
@@ -82,5 +82,5 @@ def setup_method(self, method):
     self.driver.find_element(By.ID, "password").click()
     self.driver.find_element(By.ID, "password").send_keys("password")
     self.driver.find_element(By.CSS_SELECTOR, ".mysubmit:nth-child(4)").click()
-    assert self.driver.find_element(By.CSS_SELECTOR, ".errorMessage").text == "Invalid username and password."
+    WebDriverWait(self.driver, 30).until(expected_conditions.text_to_be_present_in_element((By.CSS_SELECTOR, ".errorMessage"), "Invalid username and password."))
   
